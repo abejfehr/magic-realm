972c5bfe6756d50d0652d7b7c9c687b8bc9b1d4a
abejfehr <abe.fehr@gmail.com>
2015-02-13 14:43:54 -0500

    Keep screens in separate package

---
+++ src/com/magicrealm/client/ui/screen/Game.java
@@ -0,0 +1,34 @@
+package com.magicrealm.client.ui.screen;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Game extends JPanel {
+	/* This is to test the map */
+	/*
+	HexTile[][] mapTiles = new HexTile[6][6];
+	mapTiles[0][2] = new Cliff(4);
+	mapTiles[0][3] = new EvilValley(1);
+	mapTiles[0][4] = new HighPass(1);
+	mapTiles[1][2] = new Ledges(3);
+	mapTiles[1][3] = new Borderland(4);
+	mapTiles[1][4] = new Cavern(0);
+	mapTiles[2][1] = new Crag(2);
+	mapTiles[2][2] = new OakWoods(3);
+	mapTiles[2][3] = new BadValley(4);
+	mapTiles[2][4] = new Mountain(4);
+	mapTiles[3][0] = new DarkValley(1);
+	mapTiles[3][1] = new DeepWoods(5);
+	mapTiles[3][2] = new MapleWoods(1);
+	mapTiles[3][2] = new Caves(1);
+	mapTiles[3][3] = new PineWoods(5);
+	mapTiles[4][0] = new CurstValley(5);
+	mapTiles[4][1] = new NutWoods(2);
+	mapTiles[4][2] = new Ruins(1);
+	mapTiles[5][1] = new AwfulValley(5);
+	mapTiles[5][2] = new LindenWoods(0);
+	MapCanvas canvas = new MapCanvas(mapTiles);
+	MapScroller mapScroller = new MapScroller(canvas);
+	window.getContentPane().add(mapScroller);
+	*/
+}
