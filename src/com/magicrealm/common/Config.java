206720b5b16cb2891fcf5b3f4f040f1e7440d6b2
abejfehr <abe.fehr@gmail.com>
2015-02-13 14:44:37 -0500

    Make the main menu the default screen

--- src/com/magicrealm/common/Config.java
+++ src/com/magicrealm/common/Config.java
@@ -3,4 +3,5 @@ package com.magicrealm.common;
 public class Config {
 
 	public static final String TILE_IMAGE_LOCATION = "/res/tiles/";
+	public static final String MISC_IMAGE_LOCATION = "/res/misc/";
 }
