8d258e93e839242038fa5ed61fb06694f02df2f2
nslewa <natalyslewa@cmail.carleton.ca>
2015-02-17 13:09:35 -0500

    Add new feature to weapons: Now can check if active or not

--- src/com/magicrealm/common/weapon/Weapon.java
+++ src/com/magicrealm/common/weapon/Weapon.java
@@ -4,25 +4,34 @@ public class Weapon {
 
 	//attack, length, price
 	
+	// Players can have only one active Weapon at a time 
+	
 	/* Class Variables */
 	protected String attackType;
 	protected int length; 
-	protected int price; 
+	protected int price;
+	protected boolean isActive; 
 	
 	/* Constructor */ 
-	public Weapon(String attackType, int length, int price) {
+	public Weapon(String attackType, int length, int price, boolean isActive) {
 		this.attackType = attackType;
 		this.length = length;
-		this.price = price; 
+		this.price = price;
+		this.isActive = true; 
 	}
 	/* Get Methods */
 	public String getAttackType() { return attackType; }
 	public int getLength() { return length; }
 	public int getPrice() { return price; }
 	
+	public boolean isActive() { 
+		return isActive; 
+	} 
+	
 
  	/* Set Methods */ 
 	public void setAttackType(String a) { attackType = a; }
 	public void setLength(int l) { length = l; }
 	public void setPrice(int p) { price = p;}
+	public void setActivation(boolean a) { isActive = a; }
 }
