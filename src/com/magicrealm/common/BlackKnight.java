7a74c610a1e2196bb8be3c1e05f30c2905f142f4
theBartman <nathanbbarton@gmail.com>
2015-02-17 16:31:31 -0500

    Update Character classes/ CharacterSelectPanel

--- src/com/magicrealm/common/BlackKnight.java
+++ src/com/magicrealm/common/BlackKnight.java
@@ -1,9 +1,12 @@
 package com.magicrealm.common;
 
+import com.magicrealm.common.weapon.Mace;
+
 public class BlackKnight extends Character{
 	public BlackKnight(String name){
 		super(Vulnerability.MEDIUM, name);
 		this.setStartingPoint();
+		this.weapon = new Mace();
 	} 
 	
 	@Override
--- src/com/magicrealm/common/Captain.java
+++ src/com/magicrealm/common/Captain.java
@@ -1,9 +1,12 @@
 package com.magicrealm.common;
 
+import com.magicrealm.common.weapon.ShortSword;
+
 public class Captain extends Character{
 	public Captain(String name){
 		super(Vulnerability.MEDIUM, name);
 		//Needs to set starting point between INN, HOUSE, GUARD_HOUSE
+		this.weapon = new ShortSword();
 	}
 	
 	@Override
