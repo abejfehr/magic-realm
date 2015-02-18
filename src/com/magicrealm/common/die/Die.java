962620e80f1d9afc68b72f151575a9a4c7856edf
nslewa <natalyslewa@gmail.com>
2015-02-18 13:20:06 -0500

    Die object can be used by server

--- src/com/magicrealm/common/die/Die.java
+++ src/com/magicrealm/common/die/Die.java
@@ -1,5 +1,42 @@
 package com.magicrealm.common.die;
 
-public class Die {
+import java.io.Serializable; 
+
+public class Die implements Serializable {
+	private static final long serialVersionUID = -8806212193103753231L;
+	/** Variables **/
+	int number;
+	int id;
+	transient private boolean enabled; 
+	
+	/** Creates a new instance of Dice */
+	public Die() {
+		this.enabled = true;
+		this.roll(); 
+	}
+	
+	/** Roll function **/ 
+	public void roll() {
+		if(enabled) {
+			number = (int)(Math.random()*6) + 1;
+		}
+		
+	}
+	
+	/** Get current number on roll **/
+	public int getCurrentNumber() { return number; }
+	public int getId() { return id; }
+	
+	public void setCurrentNumber(int n) { n = number; } 
+	public void setEnabled(boolean e) { e = enabled; }
+	
+	public void reset() {
+		this.enabled = true;
+		this.roll(); 
+	}
+	
+	public String toString() {
+		return Integer.toString(number);
+	}
 
 }
