fbbe17fccfad08bc24c45d4ad72de7779adc6da4
abejfehr <abe.fehr@gmail.com>
2015-02-18 10:45:10 -0500

    Add clearing locations

--- src/com/magicrealm/common/model/hextile/Ledges.java
+++ src/com/magicrealm/common/model/hextile/Ledges.java
@@ -1,10 +1,42 @@
 package com.magicrealm.common.model.hextile;
 
+import com.magicrealm.common.model.path.Clearing;
+
 public class Ledges extends HexTile {
 	
 	public Ledges(int step) {
 		super(step, "ledges1.gif");
 
 		code = "LE";
+		
+		// Define the clearings
+		Clearing c1 = new Clearing(1, 270, 311);
+		Clearing c2 = new Clearing(2, 360, 155);
+		Clearing c3 = new Clearing(3, 376, 274);
+		Clearing c4 = new Clearing(4, 250, 211);
+		Clearing c5 = new Clearing(5, 156, 124);
+		Clearing c6 = new Clearing(6, 171, 351);
+			
+		// Define their paths
+		c3.addAdjacentByPath(c6);
+		c6.addAdjacentByPath(c3);
+
+		c6.addAdjacentByPath(c1);
+		c1.addAdjacentByPath(c6);
+
+		c1.addAdjacentByPath(c4);
+		c4.addAdjacentByPath(c1);
+
+		c2.addAdjacentByPath(c5);
+		c5.addAdjacentByPath(c2);
+		
+		// Add them to the HexTile
+		pathNodes.add(c1);
+		pathNodes.add(c2);
+		pathNodes.add(c3);
+		pathNodes.add(c4);
+		pathNodes.add(c5);
+		pathNodes.add(c6);
+
 	}
 }
--- src/com/magicrealm/common/model/hextile/PineWoods.java
+++ src/com/magicrealm/common/model/hextile/PineWoods.java
@@ -1,10 +1,26 @@
 package com.magicrealm.common.model.hextile;
 
+import com.magicrealm.common.model.path.Clearing;
+
 public class PineWoods extends HexTile {
 
 	public PineWoods(int step) {
 		super(step, "pinewoods1.gif");
 		code = "PW";
+
+		// Define the clearings
+		Clearing c2 = new Clearing(2, 120, 194);
+		Clearing c4 = new Clearing(4, 233, 81);
+		Clearing c5 = new Clearing(5, 358, 211);
+			
+		// Define their paths
+		c2.addAdjacentByPath(c4);
+		c4.addAdjacentByPath(c2);
+
+		// Add them to the HexTile
+		pathNodes.add(c2);
+		pathNodes.add(c4);
+		pathNodes.add(c5);
 
 	}
 }
