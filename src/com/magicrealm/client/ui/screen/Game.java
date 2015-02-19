d92b435762e80e0c8c31ed55925ead628ae42799
abejfehr <abe.fehr@gmail.com>
2015-02-19 13:38:18 -0500

    Start server when game is created

--- src/com/magicrealm/client/ui/screen/Game.java
+++ src/com/magicrealm/client/ui/screen/Game.java
@@ -9,34 +9,56 @@ import javax.swing.JButton;
 import com.magicrealm.client.ui.map.MapCanvas;
 import com.magicrealm.client.ui.map.MapScroller;
 import com.magicrealm.common.NetworkController;
+import com.magicrealm.common.NetworkEvents;
+import com.magicrealm.common.NetworkObserver;
 import com.magicrealm.common.model.Map;
 import com.magicrealm.common.packet.RequestMapPacket;
+import com.magicrealm.server.controller.GameController;
 
 @SuppressWarnings("serial")
-public class Game extends Screen {
-
+public class Game extends Screen implements NetworkObserver {
+	
 	public Game() {
+
 		setLayout(new BorderLayout());
 		
 		// Create a back button for testing
 		JButton test = new JButton("Back to Main Menu");
 		test.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println(scrController);
 				scrController.show(MainMenu.class);
 			}
 		});
-		add(test);
+		add(test, BorderLayout.NORTH);
 		
 		// Request the map from the server
+		NetworkController.subscribe(NetworkEvents.MAP_UPDATED, this);
 		NetworkController.sendToServer(RequestMapPacket.class);
+		
 	}
 	
 	public void drawMap(Map map) {
 		
 		MapCanvas canvas = new MapCanvas(map);
-		MapScroller mapScroller = new MapScroller(canvas);
-		add(mapScroller, BorderLayout.CENTER);
+		MapScroller scroller = new MapScroller(canvas);
+		add(scroller, BorderLayout.CENTER);
+		validate();
+		repaint();
 		
+	}
+
+	
+	
+	/*
+	 * Called whenever the network gets updated
+	 */
+	public void eventFired(int event) {
+		
+		switch(event) {
+			case NetworkEvents.MAP_UPDATED:
+				drawMap(GameController.getMap());				
+				break;
+		}
+				
 	}
 }
