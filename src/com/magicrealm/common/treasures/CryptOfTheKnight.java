6ea3be9e90bdfd659cbb3d327d52a0eae1702bf2
nslewa <natalyslewa@cmail.carleton.ca>
2015-02-21 13:17:42 -0500

    Add CryptOfTheKnight - Type of SiteCard

---
+++ src/com/magicrealm/common/treasures/CryptOfTheKnight.java
@@ -0,0 +1,21 @@
+package com.magicrealm.common.treasures;
+
+/*
+ * 	CRYPT OF THE KNIGHT
+Die	
+Roll:	Effect on the searching character:
+1	He takes the warhorse. If it is gone, he gets nothing.
+2	He takes the “T” suit of armor. If it is gone, he gets nothing.
+3	He takes the Bane sword. If it is gone, he gets nothing.
+4	He takes the treasure card. If it is gone, he gets nothing.
+5	He adds one point to his recorded Gold. These Gold points remain available when all of the treasures are gone.
+6	He must roll for a Curse.
+
+ */
+public class CryptOfTheKnight extends SiteCard {
+
+	public CryptOfTheKnight() {
+		super(false);
+	}
+
+}
