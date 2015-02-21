4c0ce32ffb4df08fa07eedb52fecfb668d296636
nslewa <natalyslewa@cmail.carleton.ca>
2015-02-21 15:12:24 -0500

    Add JPanel stuff to ActionPanel

--- src/com/magicrealm/client/ui/screen/ActionPanel.java
+++ src/com/magicrealm/client/ui/screen/ActionPanel.java
@@ -1,5 +1,30 @@
 package com.magicrealm.client.ui.screen;
 
-public class ActionPanel {
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
+import javax.swing.*;
+
+@SuppressWarnings({"unused" })
+public class ActionPanel  extends JPanel {
+
+	/*Variables */ 
+	protected JPanel actionPanel;
+	private BufferedImage block; 
+	private BufferedImage rest;
+	private BufferedImage search;
+	private BufferedImage hide; 
+	
+	
+	
+	/* Constructor */
+	public ActionPanel() { }
+	
+	
 }
