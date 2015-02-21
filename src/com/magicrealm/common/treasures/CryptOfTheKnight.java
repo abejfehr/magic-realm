0fabe0b41580dd643a440afa99f4204e40fce01c
nslewa <natalyslewa@cmail.carleton.ca>
2015-02-21 13:49:59 -0500

    Add minimal functionality to CryptOfTheKnight

--- src/com/magicrealm/common/treasures/CryptOfTheKnight.java
+++ src/com/magicrealm/common/treasures/CryptOfTheKnight.java
@@ -1,5 +1,7 @@
 package com.magicrealm.common.treasures;
 
+import com.magicrealm.common.die.Die;
+
 /*
  * 	CRYPT OF THE KNIGHT
 Die	
@@ -14,8 +16,83 @@ Roll:	Effect on the searching character:
  */
 public class CryptOfTheKnight extends SiteCard {
 
+	/* Variables */
+	private boolean active;
+	private Die die1 = new Die();
+	private Die die2 = new Die();
+	private int highestRoll;
+	
+	/* Constructor */
 	public CryptOfTheKnight() {
 		super(false);
+	}
+
+	public boolean isActive() { return active; }
+
+	public void setActive(boolean active) { this.active = active; }
+	
+	/* TODO: public void open() */
+	public void open() {
+		if((active == true) && (this.isFound() == true)) {
+			//open this card
+			highestRoll = this.highestRoll(die1, die2); 
+			
+			switch(highestRoll) {
+			case 1: case1();
+					break; 
+			case 2: case2();
+					break;
+			case 3: case3();
+					break;
+			case 4: case4();
+					break;
+			case 5: case5();
+					break;
+			case 6: case6();
+					break; 
+			default: System.out.println("Something went wrong? Can't find the highest roll"); break;
+			
+			}
+		}
+		
+	}
+
+	/* TODO: Case 6 function -  He must roll for a Curse.*/
+	private void case6() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/*TODO: Case 5 function - He adds one point to his recorded Gold. 
+	 * These Gold points remain available when all of the treasures are gone. 
+	 */
+	private void case5() {
+		// TODO Auto-generated method stub
+		
+	}
+	/*TODO: Case 4 function - He takes the treasure card. If it is gone, he gets nothing. */
+	private void case4() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* TODO: Case 3 function - He takes the Bane sword. If it is gone, he gets nothing. */
+	private void case3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* TODO: Case function - He takes the “T” suit of armor. If it is gone, he gets nothing. */
+	private void case2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* TODO: Case 1 function - He takes the warhorse. If it is gone, he gets nothing. */
+	private void case1() {
+		// TODO Auto-generated method stub
+		
+		
 	}
 
 }
